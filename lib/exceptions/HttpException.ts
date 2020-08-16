0af7219dca44fa330fceaf31d7984331d421170c
kemna <ruben-kemna@web.de>
2020-08-16 14:08:43 +0200

    default HttpException added

--- lib/exceptions/HttpException.ts
+++ lib/exceptions/HttpException.ts
@@ -1,7 +1,7 @@
 class HttpException extends Error {
     status: number;
     message: string;
-    constructor(status: number, message: string) {
+    constructor(status: number = 500, message: string = "internal Error") {
       super(message);
       this.status = status;
       this.message = message;
